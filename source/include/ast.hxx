695eb1878ded7c4b49df19eec24a8271392d9ca1
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2025-12-28 16:26:16 +0000

    fix no msvc default int types

--- source/include/ast.hxx
+++ source/include/ast.hxx
@@ -4,6 +4,15 @@
 #include <memory>
 #include <variant>
 #include <string>
+#ifdef _MSC_VER 
+// MSVC does not define u_intX_t types
+
+    #define u_int8_t uint8_t
+    #define u_int16_t uint16_t
+    #define u_int32_t uint32_t
+    #define u_int64_t uint64_t
+
+#endif // !_MSVC
 
 enum class Type
 {
